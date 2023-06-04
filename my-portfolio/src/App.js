3624ec90f13c7db3fcd49918584c8797deb492e1
Black_Rabbit <83984934+BL8CKRABBIT@users.noreply.github.com>
2023-06-04 17:50:28 -0400

    Updates

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+
+function MyButton() {
+  return (
+    <button>Click me!</button>
+  );
+}
+
+const user = {
+  Name: 'Brian Castaneda',
+  imageUrl: 'https://media.licdn.com/dms/image/C4E03AQHMlTnFYMv94g/profile-displayphoto-shrink_200_200/0/1549898066475?e=1691625600&v=beta&t=iAHu4xX9Ir2ViTAZXUx4UehGmDXHAZQeBFcosA2GynI',
+  imageSize: 90,
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="Intro">
+        <h1>{user.Name}</h1>
+        <img src={user.imageUrl} className="Avatar" alt="Avatar" style={{width: user.imageSize, height: user.imageSize}}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>This is written in React</code> <br /> Welcome to my portfolio!
+          <br />
+          <MyButton />
         </p>
         <a
           className="App-link"
@@ -21,5 +38,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
