841a57805845676695b111ad2c882de03670e7f5
Black_Rabbit <83984934+BL8CKRABBIT@users.noreply.github.com>
2023-06-04 22:20:42 -0400

    Updates to the README file

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -11,16 +11,17 @@ function MyButton() {
 const user = {
   Name: 'Brian Castaneda',
   imageUrl: 'https://media.licdn.com/dms/image/C4E03AQHMlTnFYMv94g/profile-displayphoto-shrink_200_200/0/1549898066475?e=1691625600&v=beta&t=iAHu4xX9Ir2ViTAZXUx4UehGmDXHAZQeBFcosA2GynI',
-  imageSize: 90,
+  imageSize: 300,
 };
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="Intro">
+      <header className="App-header">
         <h1>{user.Name}</h1>
-        <img src={user.imageUrl} className="Avatar" alt="Avatar" style={{width: user.imageSize, height: user.imageSize}}/>
+        <img src={user.imageUrl} className="App-logo" alt="Avatar" style={{width: user.imageSize, height: user.imageSize}}/>
         <p>
           <code>This is written in React</code> <br /> Welcome to my portfolio!
           <br />
